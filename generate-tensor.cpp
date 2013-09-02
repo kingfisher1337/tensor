ec629ef8a7784235723af11266625965ab5e0ceb
kingfisher1337 <m.rader@liwest.at>
2013-09-02 15:38:06 +0200

    Update generate-tensor.cpp
    introduced TensorBase (previously TensorSlice) as super class of Tensor

--- generate-tensor.cpp
+++ generate-tensor.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 void printInitCopyN(ostream & o, int rank) {
     for (int j = 1; j <= rank; ++j) {
-        o << "        n[" << (j-1) << "] = n" << j << ";" << endl;
+        o << "        this->n[" << (j-1) << "] = n" << j << ";" << endl;
     }
-    o << "        nTotal = n1";
+    o << "        this->nTotal = n1";
     for (int j = 2; j <= rank; ++j ) {
         o << " * n" << j;
     }
@@ -21,17 +21,17 @@ void printInitCopyN(ostream & o, int rank) {
 }
 void printInit(ostream & o, int rank) {
     printInitCopyN(o, rank);
-    o << "        m = new K[nTotal];" << endl
-      << "        if (m == 0) {" << endl
-      << "            memset(n, 0, sizeof(n));" << endl
-      << "            nTotal = 0;" << endl
+    o << "        this->m = new K[this->nTotal];" << endl
+      << "        if (this->m == 0) {" << endl
+      << "            memset(this->n, 0, sizeof(this->n));" << endl
+      << "            this->nTotal = 0;" << endl
       << "        } else {" << endl
-      << "            memset(m, 0, nTotal * sizeof(K));" << endl
+      << "            memset(this->m, 0, this->nTotal * sizeof(K));" << endl
       << "        }" << endl;
 }
 void printClean(ostream & o) {
-    o << "        if (m != 0) {" << endl
-      << "            delete[] m;" << endl
+    o << "        if (this->m != 0) {" << endl
+      << "            delete[] this->m;" << endl
       << "        }" << endl;
 }
 void printArgList(ostream & o, int rank, const string & basename = "n", bool showType = true) {
@@ -78,22 +78,24 @@ int main(int argc, char** argv) {
       << "#include <cstdlib>" << endl
       << "#include <cstring>" << endl << endl;
     
-    for (int i = 1; i <= maxRank - 1; ++i) {
-        o << "template <typename K> class TensorSlice" << i << " {" << endl
-          << "private:" << endl
+    for (int i = 1; i <= maxRank; ++i) {
+        o << "template <typename K> class TensorBase" << i << " {" << endl
+          << "protected:" << endl
           << "    K * m;" << endl
           << "    size_t n[" << i << "];" << endl
           << "    size_t nTotal;" << endl
-          << "public:" << endl
-          << "    TensorSlice" << i << "(K * m, ";
+          << "    TensorBase" << i << "() : m(0), nTotal(0) {}"
+          << "    TensorBase" << i << "(K * m, ";
         printArgList(o, i);
         o << ") : m(m) {" << endl;
         printInitCopyN(o, i);
         o << "    }" << endl
-          << "    size_t size() {" << endl
+          << "public:" << endl
+          << "    virtual ~TensorBase" << i << "() { }" << endl
+          << "    size_t size() const {" << endl
           << "        return nTotal;" << endl
           << "    }" << endl
-          << "    size_t size(size_t i) {" << endl
+          << "    size_t size(size_t i) const {" << endl
           << "        return n[i];" << endl
           << "    }" << endl
           << "    K & operator()(";
@@ -107,38 +109,37 @@ int main(int argc, char** argv) {
         printIndexMethodBody(o, i);
         o << "    }" << endl;
         for (int j = 1; j <= i - 1; ++j) {
-            o << "    TensorSlice" << (i-j) << "<K> operator()(";
+            o << "    TensorBase" << (i-j) << "<K> operator()(";
             printArgList(o, j, "i");
             o << ") {" << endl;
             printIndexMethodBodyIndexCalc(o, i, j);
-            o << "        return TensorSlice" << (i-j) << "<K>(&m[k]";
+            o << "        return TensorBase" << (i-j) << "<K>(&m[k]";
             for (int k = j; k < i; ++k) {
                 o << ", n[" << k << "]";
             }
             o << ");" << endl
               << "    }" << endl;
         }
-        o << "};" << endl << endl;
+        o << "    K * getMemory() {" << endl
+          << "        return m;" << endl
+          << "    }" << endl
+          << "};" << endl << endl;
     }
     
     for (int i = 1; i <= maxRank; ++i) {
-        o << "template <typename K> class Tensor" << i << " {" << endl
-          << "private:" << endl
-          << "    K * m;" << endl
-          << "    size_t n[" << i << "];" << endl
-          << "    size_t nTotal;" << endl
+        o << "template <typename K> class Tensor" << i << " : public TensorBase" << i << "<K> {" << endl
           << "public:" << endl
-          << "    Tensor" << i << "() : m(0), nTotal(0) {" << endl
-          << "        memset(n, 0, sizeof(n));" << endl
+          << "    Tensor" << i << "() {" << endl
+          << "        memset(this->n, 0, sizeof(this->n));" << endl
           << "    }" << endl
           << "    Tensor" << i << "(const Tensor" << i << " & t) {" << endl
-          << "        memcpy(n, t.n, sizeof(n));" << endl
-          << "        nTotal = t.nTotal;" << endl
-          << "        if (m == 0) {" << endl
-          << "            memset(n, 0, sizeof(n));" << endl
-          << "            nTotal = 0;" << endl
+          << "        memcpy(this->n, t.n, sizeof(this->n));" << endl
+          << "        this->nTotal = t.nTotal;" << endl
+          << "        if (this->m == 0) {" << endl
+          << "            memset(this->n, 0, sizeof(this->n));" << endl
+          << "            this->nTotal = 0;" << endl
           << "        } else {" << endl
-          << "            memcpy(m, t.n, nTotal * sizeof(K));" << endl
+          << "            memcpy(this->m, t.n, this->nTotal * sizeof(K));" << endl
           << "        }" << endl
           << "    }" << endl
           << "    Tensor" << i << "(";
@@ -149,13 +150,7 @@ int main(int argc, char** argv) {
           << "    ~Tensor" << i <<"() {" << endl;
         printClean(o);
         o << "    }" << endl
-          << "    size_t size() {" << endl
-          << "        return nTotal;" << endl
-          << "    }" << endl
-          << "    size_t size(size_t i) {" << endl
-          << "        return n[i];" << endl
-          << "    }" << endl
-          << "    size_t resize(size_t n1";
+          << "    void resize(size_t n1";
         for (int j = 2; j <= i; ++j) {
             o << ", size_t n" << j;
         }
@@ -163,29 +158,7 @@ int main(int argc, char** argv) {
         printClean(o);
         printInit(o, i);
         o << "    }" << endl
-          << "    K & operator()(";
-        printArgList(o, i, "i");
-        o << ") {" << endl;
-        printIndexMethodBody(o, i);
-        o << "    }" << endl
-          << "    const K & operator()(";
-        printArgList(o, i, "i");
-        o << ") const {" << endl;
-        printIndexMethodBody(o, i);
-        o << "    }" << endl;
-        for (int j = 1; j <= i - 1; ++j) {
-            o << "    TensorSlice" << (i-j) << "<K> operator()(";
-            printArgList(o, j, "i");
-            o << ") {" << endl;
-            printIndexMethodBodyIndexCalc(o, i, j);
-            o << "        return TensorSlice" << (i-j) << "<K>(&m[k]";
-            for (int k = j; k < i; ++k) {
-                o << ", n[" << k << "]";
-            }
-            o << ");" << endl
-              << "    }" << endl;
-        }
-        o << "};" << endl << endl;
+          << "};" << endl << endl;
     }
     
     o << "#endif" << endl << endl;
